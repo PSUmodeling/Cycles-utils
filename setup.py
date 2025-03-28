2b130ab2b664c2c3349931511cc05e968fdc8cdc
Yuning Shi <shiyuning@gmail.com>
2025-03-27 23:13:01 -0400

    :bookmark: Update to v1.1.0

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ with open('README.md', 'r') as f:
 
 setup(
     name='Cycles-utils',
-    version='1.0.0',
+    version='1.1.0',
     author='Yuning Shi',
     author_email="shiyuning@gmail.com",
     packages=find_packages(),
+    include_package_data=True,
     description='Python scripts to build Cycles input files and post-process Cycles output files',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -18,5 +19,8 @@ setup(
     install_requires=['numpy>=1.19.5', 'geopandas>=0.9.0', 'pandas>=1.2.4'],
     extras_require = {
         'soilgrids':  ['rioxarray>=0.5.0', 'owslib>=0.24.1', 'rasterio>=1.2.3', 'shapely>=1.7.1'],
+        'gssurgo': ['shapely>=1.7.1'],
+        'soilgrids': ['rioxarray>=0.5.0', 'owslib>=0.24.1', 'rasterio>=1.2.3', 'shapely>=1.7.1'],
+        'weather': ['shapely>=1.7.1', 'xarray>=0.20.2'],
     }
 )
