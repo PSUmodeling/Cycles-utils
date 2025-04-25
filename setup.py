d1aff14875e80150b99adf3387d4e7ae077df6dc
Yuning Shi <shiyuning@gmail.com>
2025-04-25 15:50:57 -0400

    :bookmark: Update to v1.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='Cycles-utils',
-    version='1.4.1',
+    version='1.5.0',
     author='Yuning Shi',
     author_email="shiyuning@gmail.com",
     packages=find_packages(),
@@ -15,13 +15,13 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/PSUmodeling/Cycles-utils',
     license='MIT',
-    python_requires='>=3.6',
+    python_requires='>=3.10',
     install_requires=['pandas>=1.2.4', 'geopandas>=0.9.0', 'numpy>=1.19.5'],
     extras_require = {
         'soilgrids':  ['rioxarray>=0.5.0', 'owslib>=0.24.1', 'rasterio>=1.2.3', 'shapely>=1.7.1'],
         'gssurgo': ['shapely>=1.7.1'],
         'plot': ['cartopy>=0.18.0', 'matplotlib>=3.4.2'],
         'soilgrids': ['rioxarray>=0.5.0', 'owslib>=0.24.1', 'rasterio>=1.2.3', 'shapely>=1.7.1'],
-        'weather': ['netCDF4>=1.5.7'],
+        'weather': ['netCDF4>=1.5.7', 'tqdm>=4.60.0'],
     }
 )
