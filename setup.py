df56e2746e105b06eabfc81983b08503edcad3cd
Yuning Shi <shiyuning@gmail.com>
2025-04-25 16:30:02 -0400

    :bookmark: Update to v1.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='Cycles-utils',
-    version='1.5.0',
+    version='1.5.1',
     author='Yuning Shi',
     author_email="shiyuning@gmail.com",
     packages=find_packages(),
