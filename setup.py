93f44aa357341b67550244114c8d5c70e0c15655
Yuning Shi <shiyuning@gmail.com>
2026-02-19 12:23:12 -0500

    :bookmark: Update to 3.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='Cycles-utils',
-    version='3.1.0.post',
+    version='3.1.1',
     author='Yuning Shi',
     author_email="shiyuning@gmail.com",
     packages=find_packages(),
