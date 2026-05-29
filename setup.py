2a2ae07c3d186b36f9d6beb1df451e95cbdf50dd
Yuning Shi <shiyuning@gmail.com>
2026-05-29 15:41:16 -0400

    :bookmark: Update to v4.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='Cycles-utils',
-    version='4.0.2.post',
+    version='4.0.3',
     author='Yuning Shi',
     author_email="shiyuning@gmail.com",
     packages=find_packages(),
